6615a94ea037de31a7305cdd93223ffd00d041e8
root <root@localhost.localdomain>
2026-03-30 13:55:10 +0000

    修复了版本号过低的问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.island.recorder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
